44a2371099262cd3314c96eed125ffb0086fa1db
LeeSomgyul <rmawl8600@naver.com>
2024-11-21 17:11:10 +0900

    20241121
    초기설정(mySql연결, 자바스프링부트 시작) / <홈>화면 작성중

---
+++ src/main/java/com/example/find_study_group/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.example.find_study_group.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home"; //home.html 페이지로 이동(홈)
+    }
+
+    @GetMapping("/find-study")
+    public String study(){
+        return "findStudy"; //study.html 페이지로 이동(스터디 찾기)
+    }
+
+    @GetMapping("/my-space")
+    public String mySpace(){
+        return "mySpace"; //mySpace.html 페이지로 이동(나의 학습 공간)
+    }
+
+    @GetMapping("/community")
+    public String community(){
+        return "community"; //community.html 페이지로 이동(커뮤니티)
+    }
+
+    @GetMapping("/event")
+    public String event(){
+        return "event"; //event.html 페이지로 이동(이벤트)
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login"; //login.html 페이지로 이동(로그인)
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join"; //join.html 페이지로 이동(회원가입)
+    }
+
+}
